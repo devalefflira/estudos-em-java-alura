719588ddc3a4fedbd20c32638e89be369e9ecbb1
ALEFF JUNIO SILVA LINS LIRA <dev.alefflira@gmail.com>
2022-08-10 20:31:40 -0300

    Arquivos Introdutórios, estudos sobre Java, Primeiro Commit

---
+++ 2-sintaxe_variaveis_e_fluxos/TestaPontoFlutuante.java
@@ -0,0 +1,8 @@
+
+public class TestaPontoFlutuante {
+	public static void main(String[] args) {
+		double salario;
+		salario = 1250.70;
+		System.out.println("O salário mínimo é: " + salario + " reais.");
+	}
+}
---
+++ 2-sintaxe_variaveis_e_fluxos/TestaVariaveis.java
@@ -0,0 +1,9 @@
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		System.out.println("Olá, Novo Teste");
+		int idade;
+		idade = 28;
+		System.out.println(idade);
+	}
+}
